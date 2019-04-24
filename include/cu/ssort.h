007981e40f59a74e974ea10b6b362f64c8364769
ehzx <edrickhong@gmail.com>
2019-04-24 23:03:34 +0800

    added function to remind us to implement a binary search in sorted arrays

--- include/cu/ssort.h
+++ include/cu/ssort.h
@@ -1,5 +1,9 @@
 #pragma once
 
+//Never use this please
 void BubbleSort();
 
 void RadixSort();
+
+//does a binary search in a sorted array
+s8* SortedArraySearch(s8* _restrict array,u32 elem_size,s8* _restrict key,b32 (*cmp_op)(s8* _restrict,s8* _restrict));
