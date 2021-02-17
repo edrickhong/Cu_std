e6644176b644580653f5f807c35bcbf6bdda4bc5
ehzx <edrickhong@gmail.com>
2021-02-18 04:01:38 +0800

    used fixed ttype file from master

--- include/cu/ttype.h
+++ include/cu/ttype.h
@@ -5,18 +5,20 @@
 #define _GNU_SOURCE
 #endif
 
-// MARK:C dep
+
+//MARK:C dep
 #include "stdio.h"
 #include "stdlib.h"
+
 #include "xmmintrin.h"
 
 typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned int u32;
 
-typedef char s8;
-typedef short s16;
-typedef int s32;
+typedef  char s8;
+typedef  short s16;
+typedef  int s32;
 
 typedef float f32;
 typedef double f64;
@@ -24,12 +26,12 @@ typedef double f64;
 #ifdef __WIN32
 
 typedef unsigned __int64 u64;
-typedef __int64 s64;
+typedef  __int64 s64;
 
 #else
 
 typedef unsigned long long u64;
-typedef long long s64;
+typedef  long long s64;
 
 #endif
 
@@ -53,50 +55,55 @@ typedef unsigned int ptrsize;
 #define false 0
 #endif
 
+
+
+
 //
 #ifdef _WIN32
 
 #define _deprecated __declspec(deprecated)
 #define _restrict __restrict
 #define _ainline __forceinline
-#define _align(alignment) __declspec(align(alignment))
+#define _align(alignment) __declspec( align(alignment) )
 #define _membarrier() MemoryBarrier()
 #define _winapi __stdcall
 #define _dllexport __declspec(dllexport)
-#define _packed	 // MARK: we need this to work on windows as well
+#define _packed //MARK: we need this to work on windows as well
 #define _breakpoint() DebugBreak()
 
 #else
 
-#define _deprecated __attribute__((deprecated))
+#define _deprecated __attribute__ ((deprecated))
 #define _restrict __restrict__
 #define _ainline inline __attribute__((always_inline))
-#define _align(alignment) __attribute__((aligned(alignment)))
-#define _membarrier() asm volatile("" : : : "memory")
+#define _align(alignment) __attribute__ ((aligned (alignment)))
+#define _membarrier() asm volatile ("" : : : "memory")
 #define _winapi
-#define _dllexport __attribute__((visibility("default")))
+#define _dllexport __attribute__ ((visibility ("default")))
 #define _packed __attribute__((packed))
-#define _breakpoint() __asm__ volatile("int $3\n")
+#define _breakpoint() __asm__ volatile ("int $3\n")
 
 #endif
 
+
 #define _cachealign _align(64)
 
-#define _either(condition, value1, value2) ((condition) ? value1 : value2)
+#define _either(condition,value1,value2) ((condition) ? value1: value2)
 #define _intern static
 #define _persist static
 #define _global static
-#define _countsize(size, type) size / (sizeof(type))
+#define _countsize(size,type) size/(sizeof(type))
 #define _typebitcount(a) (sizeof(a) * 8)
-#define _arraycount(ar) (sizeof(ar) / sizeof(ar[0]))
+#define _arraycount(ar) (sizeof(ar)/sizeof(ar[0]))
 #define _in_
 #define _out_
 #define _optnl_
 
-#define _writetop16(m64t, value) (m64t.u | (((u64)value) << 48))
+#define _writetop16(m64t,value) (m64t.u | (((u64)value) << 48))
 #define _gettop16(m64t) (m64t.u >> 48)
-#define _masktop16(m64t) (m64t.u ^ 0xFFFF000000000000)
+#define _masktop16(m64t)  (m64t.u ^ 0xFFFF000000000000)
 #define _zerotop16(m64t) (m64t.u ^= 0xFFFF000000000000)
+
 
 #define _unsigned_max(value) (((u64)1 << (sizeof(value) * 8)) - 1)
 #define _unsigned_max8 _unsigned_max(u8)
@@ -116,54 +123,44 @@ typedef unsigned int ptrsize;
 #define _signed_max32 _signed_max(s32)
 #define _signed_max64 _signed_max(s64)
 
-#define _kilobytes(value) ((value)*1024)
+#define _kilobytes(value) ((value) * 1024)
 #define _megabytes(value) (_kilobytes(value) * 1024)
 #define _gigabytes(value) (_megabytes((uint64_t)value) * 1024)
 #define _terrabytes(value) (_gigabytes((uint64_t)value) * 1024)
 
-#define _alignpow2(value, pow) ((value + (pow - 1)) & (~(pow - 1)))
-#define _align4(value) _alignpow2(value, 4)
-#define _align8(value) _alignpow2(value, 8)
-#define _align16(value) _alignpow2(value, 16)
-#define _align32(value) _alignpow2(value, 32)
-#define _align64(value) _alignpow2(value, 64)
-#define _align128(value) _alignpow2(value, 128)
-#define _align256(value) _alignpow2(value, 256)
+#define _alignpow2(value,pow) ((value + (pow -1)) & (~(pow -1)))
+#define _align4(value) _alignpow2(value,4)
+#define _align8(value) _alignpow2(value,8)
+#define _align16(value) _alignpow2(value,16)
+#define _align32(value) _alignpow2(value,32)
+#define _align64(value) _alignpow2(value,64)
+#define _align128(value) _alignpow2(value,128)
+#define _align256(value) _alignpow2(value,256)
 #define _devicealign(value) _align256(value)
 #define _mapalign(value) _align128(value)
 
-// kind of like rounding down by power of 2
-#define _dalignpow2(value, pow) ((value) & (~(pow - 1)))
-#define _dmapalign(value) _dalignpow2(value, 128)
+//kind of like rounding down by power of 2
+#define _dalignpow2(value,pow) ((value) & (~(pow -1)))
+#define _dmapalign(value) _dalignpow2(value,128)
 
-#define _encode_bgra(b, g, r, a) \
-	(((a)) | (((r)) << 8) | (((g)) << 16) | (((b)) << 24))
-#define _encode_rgba(r, g, b, a) \
-	(((a)) | (((b)) << 8) | (((g)) << 16) | (((r)) << 24))
-#define _encode_argb(a, r, g, b) \
-	(((b)) | (((g)) << 8) | (((r)) << 16) | (((a)) << 24))
 
+#define _encode_rgba(r,g,b,a) ((u8(r)) | ((u8(g)) << 8) | ((u8(b)) << 16) | ((u8(a)) << 24))
+#define _encode_bgra(b,g,r,a) ((u8(b)) | ((u8(g)) << 8) | ((u8(r)) << 16) | ((u8(a)) << 24))
 #define _removesignedbit(value) (value & 0x7FFFFFFF)
-#define _addsignedbit(value) (value | (1 << 31))
+#define _addsignedbit(value) (value |  (1 << 31))
 
 #ifdef DEBUG
 
-#define _kill(string, condition)                                        \
-	if ((condition)) {                                              \
-		printf("%s %s %d\n", __FUNCTION__, __FILE__, __LINE__); \
-		*(int*)0 = 0;                                           \
-	}
+#define _kill(string,condition) if((condition)) {printf("%s %s %d\n",__FUNCTION__,__FILE__,__LINE__);*(int *)0 = 0;}
 #define _dprint(string, ...) printf(string, __VA_ARGS__);
-#define _impl_PASTE(a, b) a##b
-#define _impl_CASSERT_LINE(predicate, line, file)            \
-	typedef char _impl_PASTE(assertion_failed_##file##_, \
-				 line)[2 * !!(predicate)-1];
-#define _compile_kill(condition) \
-	_impl_CASSERT_LINE(!(condition), __LINE__, __FILE__)
+#define _impl_PASTE(a,b) a##b
+#define _impl_CASSERT_LINE(predicate, line, file) \
+	typedef char _impl_PASTE(assertion_failed_##file##_,line)[2*!!(predicate)-1];
+#define _compile_kill(condition) _impl_CASSERT_LINE(!(condition),__LINE__,__FILE__)
 
 #else
 
-#define _kill(string, condition)
+#define _kill(string,condition)
 #define _compile_kill(condition)
 #define _dprint(string, ...)
 
@@ -179,6 +176,8 @@ typedef union {
 
 	s8 sbyte[4];
 	s16 sword[2];
+
+
 
 #ifdef __cplusplus
 
@@ -233,9 +232,9 @@ typedef struct {
 	void operator=(u64 o) { u = o; }
 
 	void operator=(f64 o) { f = o; }
-
 #endif
 } m64;
+
 
 void _ainline ClearPtrTop16Bits(void** ptr) {
 	m64 v = {0};
@@ -244,6 +243,14 @@ void _ainline ClearPtrTop16Bits(void** ptr) {
 	v.u ^= 0xFFFF000000000000;
 
 	*ptr = v.ptr;
+
+
+
+
+
+
+
+
 }
 
 void _ainline WritePtrTop16Bits(void** ptr, u16 value) {
@@ -255,6 +262,7 @@ void _ainline WritePtrTop16Bits(void** ptr, u16 value) {
 	v.u |= ((u64)value) << 48;
 
 	*ptr = v.ptr;
+
 }
 
 u16 _ainline GetPtrTop16Bits(void* ptr) {
@@ -262,13 +270,16 @@ u16 _ainline GetPtrTop16Bits(void* ptr) {
 	v.ptr = ptr;
 
 	return (v.u >> 48);
+
 }
 
 _ainline void* MaskPtrTop16Bits(void* ptr) {
 	ClearPtrTop16Bits(&ptr);
 
 	return ptr;
+
 }
+
 
 //This is for documentation:
 #ifdef CPP_PASS
@@ -276,3 +287,4 @@ _ainline void* MaskPtrTop16Bits(void* ptr) {
 #else
 #define REFDOC(...)
 #endif
+
